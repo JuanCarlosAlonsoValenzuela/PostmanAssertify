66b35b1b20d197e81d14ea941f3f7d5ab7bee6f7
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-03-13 13:14:09 +0100

    Implement getVariableName method
    Identical to Daikon

--- src/main/java/agora/postman/assertion/model/Invariant.java
+++ src/main/java/agora/postman/assertion/model/Invariant.java
@@ -50,32 +50,34 @@ public class Invariant {
     public String getPostmanTestCase(String parentBaseVariable, String indentationStr) {
 
         // TODO: Change this condition (hardcoded)
-        String testCaseIdentation = (!parentBaseVariable.equals("response")) ? indentationStr + "\t\t": "";
+        String testCaseIndentation = (!parentBaseVariable.equals("response")) ? indentationStr + "\t\t": "";
 
-        String res = testCaseIdentation + "// " + this.invariant + "\n";
+        String res = testCaseIndentation + "// " + this.invariant + "\n";
 
         // Test case first line
-        res = res + testCaseIdentation + "pm.test(\"" + this.invariant.replace("\"", "\\\"") + "\", () => {\n";
+        res = res + testCaseIndentation + "pm.test(\"" + this.invariant.replace("\"", "\\\"") + "\", () => {\n";
 
         // TODO: Get variable(s) value(s)
         // TODO: We are assuming only one value, with no nesting
-
+        // TODO: Modify so it can be applied to invariants with multiple variables
         // TODO: Implement, multiple functions, same method as the one used in Daikon
-        String variableName = this.variables.get(0).replace(".", "_");
-        res = res + testCaseIdentation + "\t" + variableName + " = " + parentBaseVariable + "." + variableName + ";\n";
+
+        // This method is static because an invariant can have multiple variables
+        String variableName = getPostmanVariableName(this.variables.get(0));
+        res = res + testCaseIndentation + "\t" + variableName + " = " + parentBaseVariable + "." + variableName + ";\n";
 
         // TODO: If variable is not null and not part of values to consider null
-        res = res + testCaseIdentation + "\t" + "if((" + variableName + " != null) && (!valuesToConsiderAsNull.includes(" + variableName + "))) {" + "\n";
+        res = res + testCaseIndentation + "\t" + "if((" + variableName + " != null) && (!valuesToConsiderAsNull.includes(" + variableName + "))) {" + "\n";
 
 
         // TODO: Postman assertion
-        res = res + testCaseIdentation + "\t\t" + "// " + this.postmanAssertion + ";\n";
+        res = res + testCaseIndentation + "\t\t" + "// " + this.postmanAssertion + ";\n";
 
         // Close if variable not null and not part of values to consider as null bracket
-        res = res + testCaseIdentation + "\t}\n";
+        res = res + testCaseIndentation + "\t}\n";
 
         // Close test case bracket
-        res = res + testCaseIdentation + "})\n";
+        res = res + testCaseIndentation + "})\n";
 
         return res;
 
@@ -89,6 +91,52 @@ public class Invariant {
                 ", variables=" + variables +
                 ", postmanAssertion='" + postmanAssertion + '\'' +
                 '}';
+    }
+
+    // TODO: This method must be identical to the one in Daikon
+    // TODO: Document properly (with multiple input/output example)
+    // TODO: Consider moving to a different class
+    // Returns the variable name in the format used in the Postman assertion
+    private static String getPostmanVariableName(String originalVariableName) {
+
+        // TODO: Test this if clause
+        if(!originalVariableName.startsWith("input.") &&
+                !originalVariableName.startsWith("return.") &&
+                !originalVariableName.startsWith("size(input.") &&
+                !originalVariableName.startsWith("size(return.")
+        ) {
+            throw new RuntimeException("Unexpected variable name");
+        }
+
+        String postmanVariableName = originalVariableName;
+
+        // If the variable is the size of an array
+        if(postmanVariableName.startsWith("size(")) {
+
+            // Remove "size(" (at the start) and ")" (at the end) characters
+            postmanVariableName = postmanVariableName.substring("size(".length(), postmanVariableName.length() - 1);
+
+            // Add suffix
+            postmanVariableName = postmanVariableName + "_size";
+        }
+
+        // Remove array special characters and add a suffix indicating that the variable is an array
+        if (postmanVariableName.contains("[]") || postmanVariableName.contains("[..]")) {
+
+            // Remove characters
+            postmanVariableName = postmanVariableName.replace("[]", "");
+            postmanVariableName = postmanVariableName.replace("[..]", "");
+
+            // Add suffix
+            postmanVariableName = postmanVariableName + "_array";
+        }
+
+        // Replace variable hierarchy separator with snake_case
+        postmanVariableName = postmanVariableName.replace(".", "_");
+
+
+        return postmanVariableName;
+
     }
 
 }
