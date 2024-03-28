b106b259612482855e13eba912a00a33d5d8f54e
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-03-28 20:07:19 +0100

    Add Javadoc

--- src/main/java/agora/postman/assertion/debug/DebugUtils.java
+++ src/main/java/agora/postman/assertion/debug/DebugUtils.java
@@ -5,7 +5,12 @@ package agora.postman.assertion.debug;
  */
 public class DebugUtils {
 
-    // TODO: Document
+    /**
+     *
+     * @param variableName: Name of the variable in Postman format
+     * @param indentation: Indentation level
+     * @return Postman test script snippet that prints the name and the value of the provided variable
+     */
     public static String printVariableValueScript(String variableName, String indentation) {
         return indentation + "// Printing value of " + variableName + " variable\n"
                 + indentation + "console.log(\"Printing value of " + variableName + "\");\n"
